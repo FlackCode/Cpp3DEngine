fabceca49a90a632e7f245b12bd876bd71a9e968
FlackCode <fl4ckc0de@gmail.com>
2024-12-25 10:08:09 +0200

    Init Commit

---
+++ Cpp3DEngine.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "olcConsoleGameEngine.h"
+
+class olcEngine3D : olcConsoleGameEngine {
+public:
+    olcEngine3D() {
+
+    }
+public:
+    bool OnUserCreate() override {
+
+    }
+
+    bool OnUserUpdate(float fElapsedTime) override {
+
+    }
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
